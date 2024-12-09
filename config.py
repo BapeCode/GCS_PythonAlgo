30be4432689709b8d0678132b80f520863311e24
Bape <154688709+BapeCode@users.noreply.github.com>
2024-12-09 10:18:48 +0100

    Init app
    Add config

---
+++ config.py
@@ -0,0 +1,9 @@
+config = {
+    'project_id': 'Innova - Manager',
+    'db_name': 'root',
+    'db_password': 'root',
+    'db_host': 'localhost',
+    'db_port': '3306',
+    'file_path': "./data/"
+}
+
---
+++ menu.py
@@ -0,0 +1,10 @@
+def menu():
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5. Exit")
+    choice = int(input("Enter your choice: "))
+    return choice
+
+menu()
